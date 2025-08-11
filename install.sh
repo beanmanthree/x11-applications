2cfe9e6775ea8da527dc7c8f046f445de05b5654
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-11 07:58:28 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 # Install X Virtual Framebuffer and other required stuff
 sudo apt update -y
 sudo apt install -y xvfb x11vnc fluxbox gedit unzip
-sudo apt install -y dotnet-runtime-7.0 libicu-dev libfontconfig1
-sudo apt install -y libopenal1 libopenal-dev libgdiplus libx11-6 libxrandr2 libxi6 libxcursor1 libxinerama1
+sudo apt install -y libopenal1 libopenal-dev
 
 # Download noVNC
 cd ~
