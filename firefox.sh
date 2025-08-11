10bbf887324557d55dda4bb42227e1290188fcea
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-11 08:58:58 -0600

    Create firefox.sh

---
+++ firefox.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Check if installed
+if ! dpkg -l | grep -q "firefox-esr"; then
+  
+  # Install firefox-esr
+  sudo add-apt-repository ppa:mozillateam/ppa -y
+  sudo apt-get install firefox-esr
+fi
+
+# Run firefox-esr
+export ALSOFT_DRIVERS=null
+export DISPLAY=:99
+firefox-esr &
