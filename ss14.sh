a8c39e77f47172c93386354c464c0a9c88dbc627
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-10 22:23:04 -0600

    Update ss14.sh

--- ss14.sh
+++ ss14.sh
@@ -2,19 +2,19 @@
 
 # Check if exists
 if [ ! -d "$HOME/applications/ss14" ]; then
-    
-    # Make folder
-    mkdir -p ~/applications/ss14
-    cd ~/applications/ss14
-    
-    # Download ss14
-    wget https://github.com/space-wizards/SS14.Launcher/releases/latest/download/SS14.Launcher_Linux.zip
-    
-    # Extract
-    unzip SS14.Launcher_Linux.zip
-    
-    # Prepare file
-    chmod +x SS14.Launcher
+  
+  # Make folder
+  mkdir -p ~/applications/ss14
+  cd ~/applications/ss14
+  
+  # Download ss14
+  wget https://github.com/space-wizards/SS14.Launcher/releases/latest/download/SS14.Launcher_Linux.zip
+  
+  # Extract
+  unzip SS14.Launcher_Linux.zip
+  
+  # Prepare file
+  chmod +x SS14.Launcher
 fi
 
 # Run ss14
