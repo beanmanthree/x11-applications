5315faf696333a18795e9dd7133f98c7cce6343e
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-10 23:01:08 -0600

    Update install_x11.sh

--- install_x11.sh
+++ install_x11.sh
@@ -2,7 +2,7 @@
 
 # Install X Virtual Framebuffer and other required stuff
 sudo apt update -y
-sudo apt install -y xvfb x11vnc fluxbox firefox gedit unzip
+sudo apt install -y xvfb x11vnc fluxbox gedit unzip
 sudo apt install -y dotnet-runtime-7.0 libicu-dev libfontconfig1
 sudo apt install -y libopenal1 libopenal-dev libgdiplus libx11-6 libxrandr2 libxi6 libxcursor1 libxinerama1
 
