07e5664d910c327bab2cab0452669495952dfe39
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-10 22:12:20 -0600

    Update ss14.sh

--- ss14.sh
+++ ss14.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+# Make folder
+mkdir -p ~/applications/ss14
+cd ~/applications/ss14
+
+# Download ss14
+wget https://github.com/space-wizards/SS14.Launcher/releases/latest/download/SS14.Launcher_Linux.zip
+
+# Extract
+unzip SS14.Launcher_Linux.zip
+
+# Prepare file
+chmod +x SS14.Launcher
