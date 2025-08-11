08288ffb08bf4b6cb2583f808223edde009eba71
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-11 12:57:40 -0600

    Create mindustry.sh

---
+++ mindustry.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if mindustry exists
+if [ ! -d "$HOME/applications/mindustry" ]; then
+  
+  # Make folder
+  mkdir -p ~/applications/mindustry
+  cd ~/applications/mindustry
+
+  # Download mindustry
+  wget https://github.com/beanmanthree/mindustry-linux-download/releases/download/release/mindustry-linux-64-bit.zip
+
+  # Extract
+  unzip mindustry-linux-64-bit.zip
+
+  # Prepare file
+  chmod +x Mindustry
+fi
+
+# Run mindustry
+export ALSOFT_DRIVERS=null
+export DISPLAY=:99
+~/applications/mindustry/mindustry &
