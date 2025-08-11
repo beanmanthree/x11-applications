a80103182942b86eacd3437ada4cf6946405bd31
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-10 22:38:17 -0600

    Update install_x11.sh

--- install_x11.sh
+++ install_x11.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Install X Virtual Framebuffer
-sudo apt update
-sudo apt install -y xvfb x11vnc fluxbox firefox gedit unzip libopenal1 libopenal-dev libgdiplus libx11-6 libxrandr2 libxi6 libxcursor1 libxinerama1 dotnet-runtime-7.0 libicu-dev libfontconfig1
+# Install X Virtual Framebuffer and other required stuff
+sudo apt update -y
+sudo apt install -y xvfb x11vnc fluxbox firefox gedit unzip
+sudo apt install -y dotnet-runtime-7.0 libicu-dev libfontconfig1
+sudo apt install -y libopenal1 libopenal-dev libgdiplus libx11-6 libxrandr2 libxi6 libxcursor1 libxinerama1
 
 # Download noVNC
 cd ~
