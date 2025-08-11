2d5aa94daa068be3cb17b918d8bd36a9babb00f2
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-10 22:21:04 -0600

    Update ss14.sh

--- ss14.sh
+++ ss14.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Make folder
-mkdir -p ~/applications/ss14
-cd ~/applications/ss14
+# Check if exists
+if [ ! -d "$HOME/applications/ss14" ]; then
+    
+    # Make folder
+    mkdir -p ~/applications/ss14
+    cd ~/applications/ss14
+    
+    # Download ss14
+    wget https://github.com/space-wizards/SS14.Launcher/releases/latest/download/SS14.Launcher_Linux.zip
+    
+    # Extract
+    unzip SS14.Launcher_Linux.zip
+    
+    # Prepare file
+    chmod +x SS14.Launcher
+fi
 
-# Download ss14
-wget https://github.com/space-wizards/SS14.Launcher/releases/latest/download/SS14.Launcher_Linux.zip
-
-# Extract
-unzip SS14.Launcher_Linux.zip
-
-# Prepare file
-chmod +x SS14.Launcher
+# Run ss14
+export ALSOFT_DRIVERS=null
+export DISPLAY=:99
+~/applications/ss14/SS14.Launcher &
