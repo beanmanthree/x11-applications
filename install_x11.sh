29b708fe4c0acfa05a996cb2c354d8f450633486
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-10 22:09:01 -0600

    Update install_x11.sh

--- install_x11.sh
+++ install_x11.sh
@@ -3,4 +3,10 @@
 # Install X Virtual Framebuffer
 sudo apt update
 sudo apt install -y xvfb x11vnc fluxbox firefox gedit unzip libopenal1 libopenal-dev libgdiplus libx11-6 libxrandr2 libxi6 libxcursor1 libxinerama1 dotnet-runtime-7.0 libicu-dev libfontconfig1
+
+# Download noVNC
+cd ~
+git clone https://github.com/novnc/noVNC.git
+
+#Install websockify
 pip3 install websockify
