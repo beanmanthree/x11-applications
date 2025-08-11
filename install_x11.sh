2c7da7961f1729829f8be0260b60afb1321dfb69
Ivan Chang <80505615+beanmanthree@users.noreply.github.com>
2025-08-10 22:05:38 -0600

    Update install_x11.sh

--- install_x11.sh
+++ install_x11.sh
@@ -2,5 +2,5 @@
 
 # Install X Virtual Framebuffer
 sudo apt update
-sudo apt install -y xvfb x11vnc fluxbox firefox gedit
+sudo apt install -y xvfb x11vnc fluxbox firefox gedit unzip libopenal1 libopenal-dev libgdiplus libx11-6 libxrandr2 libxi6 libxcursor1 libxinerama1 dotnet-runtime-7.0 libicu-dev libfontconfig1
 pip3 install websockify
